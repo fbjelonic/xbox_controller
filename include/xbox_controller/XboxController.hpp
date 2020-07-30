0af4174d9e9f66314306b874d4d08040450434f4
Filip Bjelonic <filip.bjelonic@gmail.com>
2020-07-30 17:50:59 +0200

    Some changes

--- include/xbox_controller/XboxController.hpp
+++ include/xbox_controller/XboxController.hpp
@@ -5,6 +5,16 @@
 #include "std_msgs/Int16.h"
 #include "sensor_msgs/Joy.h"
 #include "geometry_msgs/Twist.h"
+#include "sensor_msgs/Image.h"
+#include "sensor_msgs/image_encodings.h"
+#include <boost/shared_ptr.hpp>
+
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 // #include "xbox_controller/Engine.h"
 
 namespace xbox_controller {
@@ -14,6 +24,7 @@ public:
   XboxController(ros::NodeHandle& nodeHandle);
   virtual ~XboxController();
   void joyCallback(const sensor_msgs::Joy& msg);
+  void imageCallback(const sensor_msgs::Image& img);
 
 private:
   bool readParameters();
@@ -33,6 +44,8 @@ private:
   bool lt_used_;
   bool rt_used_;
 
+  cv::Mat image_;
+
   std_msgs::Int16 left_wheel_;
   std_msgs::Int16 right_wheel_;
   std_msgs::UInt8 servo_angle_;
@@ -40,6 +53,8 @@ private:
   ros::Publisher raspi_pub_;
   ros::Subscriber joy_sub_;
   ros::NodeHandle nodeHandle_;
+  image_transport::ImageTransport it_;
+  ros::Subscriber image_sub_;
 };
 
 }
--- src/XboxController.cpp
+++ src/XboxController.cpp
@@ -3,13 +3,29 @@
 namespace xbox_controller {
 
 XboxController::XboxController(ros::NodeHandle& nodeHandle) :
-  lt_used_(false), rt_used_(false), nodeHandle_(nodeHandle)
+  lt_used_(false), rt_used_(false), nodeHandle_(nodeHandle), it_(nodeHandle_)
 {
+  //cv::VideoCapture cap;
+  //cap.open(0);
+  //cap.read(image_);
+
+  //out_msg_->image = image_;
+  //out_msg_->header.stamp = ros::Time::now();
+  //out_msg_->header.frame_id = "world";
+  //out_msg_->header.seq = 24;
+  //out_msg_->encoding = sensor_msgs::image_encodings::BGR8;
+
   // publish messages for raspberry pi (for 2wd arduino)
   raspi_pub_ = nodeHandle_.advertise<geometry_msgs::Twist>("engine", 10);
 
   // get controller input from joy topic
   joy_sub_ = nodeHandle_.subscribe("/joy", 10, &XboxController::joyCallback, this);
+
+  // publish image to yolo_ros
+  image_sub_ = nodeHandle_.subscribe("/darknet_ros/detection_image", 1, &XboxController::imageCallback, this);
+  //image_pub_ = it_.advertise("/camera/rgb/image_raw", 1);
+  ROS_WARN("Sending mesage now:");
+  ROS_WARN("Finished sending messages");
 
   servo_angle_.data = 90;
   left_wheel_.data = 0;
@@ -47,6 +63,9 @@ XboxController::~XboxController()
 
 void XboxController::joyCallback(const sensor_msgs::Joy& msg)
 {
+  //cv::imshow("Live", image_);
+  //cv::waitKey(0);
+  //image_pub_.publish(cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::BGR8, image_).toImageMsg());
   // extract value of xbox controller
   double rightJoystickValue = static_cast<double>(msg.axes.at(static_cast<size_t>(right_joystick_)));
   double leftJoystickValue = static_cast<double>(msg.axes.at(static_cast<size_t>(left_joystick_)));
@@ -57,6 +76,14 @@ void XboxController::joyCallback(const sensor_msgs::Joy& msg)
   handleDiffdrive(rt_buttonValue, lt_buttonValue, leftJoystickValue);
 
   sendRequest();
+}
+
+void XboxController::imageCallback(const sensor_msgs::Image &img)
+{
+  cv_bridge::CvImagePtr ptr;
+  ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
+  cv::imshow("Live", ptr->image);
+  cv::waitKey(0);
 }
 
 void XboxController::sendRequest()
